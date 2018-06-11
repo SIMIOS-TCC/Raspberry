7b24dad899b8a346b9e60f8345eafcb3c56c73ca
POGVaz <pedro.oscar.vaz@usp.br>
2018-06-11 16:19:10 +0000

    Organizacao da pasta

---
+++ src/teste.py
@@ -0,0 +1,19 @@
+from QueriesMYSQL import *
+
+# Variaveis para testar as conexoes
+colunasExemplo = [["ID", "INT NOT NULL AUTO_INCREMENT"], ["nome", "VARCHAR(255) NULL"], ["sobrenome", "VARCHAR(255) NULL"]]
+colunasInserir = ["nome", "sobrenome"]
+valoresExemplo = [["Tharin", "Vaalanor"], ["Badon", "Patafirme"], ["Eiguer", "Caolho"]]
+
+def main():
+    criar("teste", colunasExemplo)
+
+    inserir("teste", colunasInserir, valoresExemplo)
+
+    ler("teste", colunasInserir)
+
+    raw_input("Pressione ENTER para deletar")
+
+    deletar("teste")
+
+    #ler("simio", ["*"])
